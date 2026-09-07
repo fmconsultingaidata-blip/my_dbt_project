49a25bcce90557d2b9d60997c982b6aac491858a
Joseph Shump <schumpeterjoseph01@gmail.com>
2026-09-07 16:27:03 +0000

    mod

--- models/staging/sales_database/stg_sales_database__order_item.sql
+++ models/staging/sales_database/stg_sales_database__order_item.sql
@@ -1,11 +1,10 @@
 select CONCAT(order_id, '_', product_id) AS order_item_id,
- 1 AS column_1_y,
- order_id as order_id_y,
- product_id as product_id_y,
- seller_id as seller_id_y,
- DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at_x,
- price as unit_price_y,
- shipping_cost as shipping_cost_y,
- quantity as item_quantity_y,
- (price * quantity) + shipping_cost as total_order_item_amount_y
+ order_id as order_id,
+ product_id as product_id,
+ seller_id as seller_id,
+ DATETIME(pickup_limit_date, "Europe/Paris") AS picked_up_limited_at,
+ price as unit_price,
+ shipping_cost as shipping_cost,
+ quantity as item_quantity,
+ (price * quantity) + shipping_cost as total_order_item_amount
 from {{ source('sales_database', 'order_item') }}
